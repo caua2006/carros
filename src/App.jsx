5e8a4ae12f93673703fa977702ea74c8c7f33640
caua2006 <caua2006carvalho@gmail.com>
2024-03-18 09:25:29 -0300

    3 commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ export default function App() {
   return (
    <main>
       <section>
-        <div>
+        <div className='bg-orange'>
           <img src={iconSedans} alt="Icon sedan car" />
           <h2 className='text-3xl'>Sedans</h2>
           <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/ofertas/feiroes/lojaoficialbmw/carros-usados/estoque/bmw?lkid=2243&tipoveiculo=carros-usados&marca1=bmw&feirao=Loja%20Oficial%20BMW" target='_blank'>Ver mais</a>
         </div>
         
-        <div>
+        <div className='bg-green'>
           <img src={iconSuvs} alt="" />
           <h2>SUVs</h2>
           <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/suvs" target='_blank'>Ver mais</a>
         </div>
 
-        <div>
+        <div className='bg-dark-green'>
           <img src={iconLuxury} alt="" />
           <h2>Luxuosos</h2>
           <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
