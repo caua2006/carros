a2ad4ed4b3087447e61ecdf72b2ac442702444d9
caua2006 <caua2006carvalho@gmail.com>
2024-03-18 11:03:45 -0300

    11 commit do app: Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import Suvs from "./components/Suvs";
 export default function App() {
   return (
    <main className='min-h-svh bg-offwhitebg px-6 py-20 antialiased lg:flex justify-center items-center'>
-      <section className='lg:flex max-w-[920px]'>
+      <div className='lg:flex max-w-[920px]'>
        <Sedans/>
        <Suvs/>
        <Luxury/>
-      </section>
+      </div>
    </main>
   )
 }
--- src/components/Luxury.jsx
+++ src/components/Luxury.jsx
@@ -2,11 +2,11 @@ import iconLuxury from '../assets/icon-luxury.svg'
 
 export default function Luxury() {
   return (
-    <div className='bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-e-lg'>
+    <section className='bg-dark-green p-12 max-lg:rounded-b-lg lg:rounded-e-lg'>
           <img src={iconLuxury} alt="" className='mb-7' />
           <h2 className='font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg' >Luxuosos</h2>
           <p className='font-lexend mb-7 text-offwhite text-sm lg:mb-20'>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
           <a className='font-lexend bg-offwhitebg text-dark-green px-10 py-3 border-2 border-offwhitebg rounded-3xl block w-fit hover:bg-dark-green hover:text-white' href="https://www.webmotors.com.br/comprar/ferrari/296-gts/30-v6-turbo-phev-f1-dct/2-portas/2023/50525519?pos=a50525519g:&np=1">Ver mais</a>
-    </div>
+    </section>
   )
 }
